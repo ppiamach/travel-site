fa5025d70ee2856aa265f27784c30c4386fa29d2
Petr <pedro.iamachkine@gmail.com>
2019-02-19 21:19:44 -0500

    implemented moble-first by adding 'mixins' (postcss-mixins)

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,12 +3,13 @@ var gulp = require('gulp'),
 	autoprefixer = require('autoprefixer-core'),
 	cssvars = require('postcss-simple-vars'),
 	cssnested = require('postcss-nested'),
-	cssImport = require('postcss-import');
+	cssImport = require('postcss-import'),
+	mixins = require('postcss-mixins');
 
 gulp.task('styles', function() {
 	return gulp
 		.src('./app/assets/styles/styles.css')
-		.pipe(postcss([cssImport, cssnested, cssvars, autoprefixer({ browsers: ['> 0%'] })]))
+		.pipe(postcss([cssImport, mixins, cssnested, cssvars, autoprefixer({ browsers: ['> 0%'] })]))
 		.on('error', function(errorInfo) {
 			console.log(errorInfo.toString());
 			this.emit('end');
