16d763a11836a779ab2e1a605f0b77e32e04b27e
Petr <pedro.iamachkine@gmail.com>
2019-02-19 20:57:22 -0500

    separated gulp tasks into separate files and linked to gulpfile.js Also, added errorHandling with .on (styles.js)

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp'),
+	postcss = require('gulp-postcss'),
+	autoprefixer = require('autoprefixer-core'),
+	cssvars = require('postcss-simple-vars'),
+	cssnested = require('postcss-nested'),
+	cssImport = require('postcss-import');
+
+gulp.task('styles', function() {
+	return gulp
+		.src('./app/assets/styles/styles.css')
+		.pipe(postcss([cssImport, cssnested, cssvars, autoprefixer({ browsers: ['> 0%'] })]))
+		.on('error', function(errorInfo) {
+			console.log(errorInfo.toString());
+			this.emit('end');
+		})
+		.pipe(gulp.dest('./app/parsed/styles'));
+});
