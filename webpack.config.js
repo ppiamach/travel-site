3b3b9ee761054fe1b803c019d23793464f1353b8
Petr <pedro.iamachkine@gmail.com>
2019-03-26 18:31:58 -0300

    Implemented Lazyload functionality

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,28 @@
 var path = require('path');
 
 module.exports = {
-	entry  : './app/assets/scripts/app.js',
+	entry  : {
+		app    : './app/assets/scripts/app.js',
+		Vendor : './app/assets/scripts/Vendor.js'
+	},
 	output : {
 		path     : path.resolve(__dirname, './app/parsed/scripts'),
-		filename : 'App.js'
-    },
-    module: {
-        rules: [
-          {
-            test: /\.js$/,
-            exclude: /(node_modules|bower_components)/,
-            use: {
-              loader: 'babel-loader',
-              options: {
-                presets: ['@babel/preset-env']
-              }
-            }
-          }
-        ]
-      }
+		filename : '[name].js'
+	},
+	module : {
+		rules : [
+			{
+				test    : /\.js$/,
+				exclude : /(node_modules|bower_components)/,
+				use     : {
+					loader  : 'babel-loader',
+					options : {
+						presets : ['@babel/preset-env']
+					}
+				}
+			}
+		]
+	}
 };
-
 
 // ./ refers to current path which is the travel-site folder
